3d15d8866d587b25b6fec710bbe0f92f0107ce92
signor1 <emmanuelomemgboji@gmail.com>
2024-02-26 19:25:50 +0100

    feat: updated

--- contracts/MyNFTMarket.sol
+++ contracts/MyNFTMarket.sol
@@ -19,17 +19,21 @@ contract NFTMarketplace is ERC721, Ownable {
 
     function mint(address to) external onlyOwner {
         uint256 tokenId = nextTokenId++;
+
         _safeMint(to, tokenId);
     }
 
     function buy(uint256 tokenId) external payable {
         require(_exists(tokenId), "Token ID does not exist");
+
         require(listings[tokenId].isForSale, "Token is not for sale");
+
         require(msg.value >= listings[tokenId].price, "Insufficient funds");
 
         address seller = listings[tokenId].seller;
         listings[tokenId].isForSale = false;
         listings[tokenId].price = 0;
+
         _transfer(seller, msg.sender, tokenId);
 
         payable(seller).transfer(msg.value);
@@ -37,6 +41,7 @@ contract NFTMarketplace is ERC721, Ownable {
 
     function listForSale(uint256 tokenId, uint256 price) external {
         require(_exists(tokenId), "Token ID does not exist");
+
         require(
             ownerOf(tokenId) == msg.sender,
             "You are not the owner of this token"
@@ -51,6 +56,7 @@ contract NFTMarketplace is ERC721, Ownable {
 
     function removeListing(uint256 tokenId) external {
         require(_exists(tokenId), "Token ID does not exist");
+
         require(
             ownerOf(tokenId) == msg.sender,
             "You are not the owner of this token"
@@ -61,6 +67,7 @@ contract NFTMarketplace is ERC721, Ownable {
 
     function setPrice(uint256 tokenId, uint256 price) external {
         require(_exists(tokenId), "Token ID does not exist");
+
         require(
             ownerOf(tokenId) == msg.sender,
             "You are not the owner of this token"
