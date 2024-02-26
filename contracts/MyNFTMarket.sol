d64e089d6f7c8bfb3f4a8291fb2cd852e65238f3
signor1 <emmanuelomemgboji@gmail.com>
2024-02-26 19:30:30 +0100

    feat: contract update

--- contracts/MyNFTMarket.sol
+++ contracts/MyNFTMarket.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTMarketplace is ERC721, Ownable {
+contract MyNFTMarket is ERC721, Ownable {
     uint256 public nextTokenId;
 
     struct Listing {
